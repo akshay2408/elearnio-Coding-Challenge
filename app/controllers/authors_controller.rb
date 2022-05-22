94d399910d3b5721190532f8cce0d7ded76320f1
akshay2408 <akshaybabar2408@gmail.com>
2022-05-23 04:30:00 +0530

    Added course controller

---
+++ app/controllers/authors_controller.rb
@@ -0,0 +1,41 @@
+class AuthorsController < ApplicationController
+  before_action :set_author, only: %i[show update destroy]
+  def index
+    @authors = Author.all
+    render json: @authors, status: :ok
+  end
+
+  def show
+    render json: @author, status: :ok
+  end
+
+  def create
+    @author = Author.create(author_params)
+    render json: @author, status: :created
+  end
+
+  def update
+    @author.update(author_params)
+    render json: @author, status: :ok
+  end
+
+  def destroy
+
+    if Author.count > 1
+      @author.update_talents
+      @author.destroy
+      render json: {message: "Author is successfully destroyed"}
+    end
+    render json:{error: "author is not able to destroy"}
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name, :bio, :dob)
+  end
+
+  def set_author
+    @author = Author.find(params[:id])
+  end
+end
