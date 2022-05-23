00832dfe9b25aa97239ebfcf8ec4eda2c7ba5f0b
akshay2408 <akshaybabar2408@gmail.com>
2022-05-23 14:32:01 +0530

    added talents controller

---
+++ app/controllers/talents_controller.rb
@@ -0,0 +1,37 @@
+class TalentsController < ApplicationController
+  before_action :set_talent, only: %i[show update destroy]
+  def index
+    @talents = Talent.all
+    render json: @talents, status: :ok
+  end
+
+  def show
+    render json: @talent, status: :ok
+  end
+
+  def create
+    @talent = Talent.create(talent_params)
+    render json: @talent, status: :created
+  end
+
+  def update
+    @talent.update(talent_params)
+    render json: @talent, status: :ok
+  end
+
+  def destroy
+    @talent.destroy
+    render json: {message: "Talent is successfully destroyed"}, status: :ok
+  end
+
+  private
+
+  def talent_params
+    params.require(:talent)
+          .permit(:skill, :description, :author_id, :course_id)
+  end
+
+  def set_talent
+    @talent = Talent.find(params[:id])
+  end
+end
